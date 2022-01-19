3c5992f1e1e5dc9aa2071362c7baa0f712694749
XDeSIG <xdesig@gmail.com>
2022-01-19 21:25:17 +0100

    Update test teclado

---
+++ E_KEY/E_KEYPAD/Escornabot_test_teclado/Escornabot_test_teclado.ino
@@ -0,0 +1,25 @@
+/*
+  AnalogReadSerial (modficado XDeSIG)
+  This example code is in the public domain.
+*/
+
+// A7 Escornabot Singularis (2.12)
+#define input A0 //A7 
+
+int sensorValue;
+int sensorValue_old;
+
+void setup() {
+  Serial.begin(9600);
+ pinMode(input,INPUT); 
+}
+
+void loop() {
+  sensorValue = analogRead(input);
+
+  if (sensorValue_old != sensorValue){
+  Serial.println(sensorValue);
+  }
+  sensorValue_old = sensorValue;
+  delay(100);        
+}
