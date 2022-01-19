27a1028638bc03cb8b53573cf8bb30ac805f6a1e
XDeSIG <xdesig@gmail.com>
2022-01-19 21:34:38 +0100

    Update Escornabot_test_teclado.ino

--- E_KEY/E_KEYPAD/Escornabot_test_teclado/Escornabot_test_teclado.ino
+++ E_KEY/E_KEYPAD/Escornabot_test_teclado/Escornabot_test_teclado.ino
@@ -4,22 +4,23 @@
 */
 
 // A7 Escornabot Singularis (2.12)
-#define input A7 
+#define input A7
 
 int sensorValue;
 int sensorValue_old;
 
 void setup() {
   Serial.begin(9600);
- pinMode(input,INPUT); 
+  pinMode(input, INPUT);
+  Serial.println ("press a button");
 }
 
 void loop() {
   sensorValue = analogRead(input);
 
-  if (sensorValue_old != sensorValue){
-  Serial.println(sensorValue);
+  if (sensorValue_old != sensorValue) {
+    Serial.println(sensorValue);
   }
   sensorValue_old = sensorValue;
-  delay(100);        
+  delay(100);
 }
